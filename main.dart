cc903d26f10fe3d40e4d7ca9553fd914139633e4
UndertaleXFrisk <eng.vireaksathya@gmail.com>
2025-11-21 23:19:47 +0700

    done

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'w7_statefull/EX-1/ex_1.dart';
+import 'w7_statefull/EX-2/ex_2_step_1_start.dart';
+import 'w7_statefull/EX-3/ex_3_start.dart';
+import 'w7_statefull/Ex-4/ex_4.dart';
+void main() {
+  runApp(const MaterialApp(
+    //home: Ex1(),
+    //home: Ex2(),
+    //home: Ex3(),
+    home: Ex4(),
+  ));
+}
+
